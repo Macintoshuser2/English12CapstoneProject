b30cd6ddeebe511cd0f121a725aebce748306763
Macintoshuser2 <joshuarootsoniczg@gmail.com>
2018-05-05 22:36:21 -0500

    Finished Crafting Recipes, added navigation for easy navigation of recipes page.

--- change_image.js
+++ change_image.js
@@ -105,6 +105,89 @@ window.addEventListener("load", (event) => {
         "textures/gold_ingot.png", 
         "textures/leather.png"
     ];
+
+    let dyeFlowersImages = 
+    [
+        "textures/flower_allium.png", 
+        "textures/flower_blue_orchid.png",
+        "textures/flower_dandelion.png", 
+        "textures/flower_houstonia.png", 
+        "textures/flower_oxeye_daisy.png", 
+        "textures/flower_paeonia.png", 
+        "textures/flower_rose.png", 
+        "textures/flower_tulip_orange.png", 
+        "textures/flower_tulip_pink.png", 
+        "textures/flower_tulip_red.png", 
+        "textures/flower_tulip_white.png",  
+    ];
+
+    let doorMaterialsImages = 
+    [
+        "textures/planks_acacia.png", 
+        "textures/planks_big_oak.png",
+        "textures/planks_birch.png", 
+        "textures/planks_jungle.png",
+        "textures/planks_oak.png", 
+        "textures/planks_spruce.png", 
+        "textures/iron_ingot.png"
+    ];
+
+    let minecartWithImages = 
+    [
+        "textures/hopper.png",
+        "textures/furnace_front_off.png",
+        "textures/chest.png",
+        "textures/tnt_side.png"
+    ];
+
+    let pressurePlateImages =
+    [
+        "textures/stone.png",
+        "textures/iron_ingot.png",
+        "textures/gold_ingot.png", 
+        "textures/planks_acacia.png", 
+        "textures/planks_big_oak.png",
+        "textures/planks_birch.png", 
+        "textures/planks_jungle.png",
+        "textures/planks_oak.png", 
+        "textures/planks_spruce.png"
+    ];
+
+    let slabImages = 
+    [
+        "textures/cobblestone.png",
+        "textures/planks_acacia.png", 
+        "textures/planks_big_oak.png",
+        "textures/planks_birch.png", 
+        "textures/planks_jungle.png",
+        "textures/planks_oak.png", 
+        "textures/planks_spruce.png",
+        "textures/stonebrick.png",
+        "textures/quartz_block_bottom.png",
+        "textures/stone.png"
+    ];
+
+    let buttonImages = 
+    [
+        "textures/planks_oak.png",
+        "textures/planks_acacia.png", 
+        "textures/planks_big_oak.png",
+        "textures/planks_birch.png", 
+        "textures/planks_jungle.png",
+        "textures/planks_oak.png", 
+        "textures/planks_spruce.png",
+        "textures/stone.png"
+    ];
+
+    let woodLogImages =
+    [
+        "textures/log_acacia_top.png",
+        "textures/log_big_oak_top.png",
+        "textures/log_birch_top.png",
+        "textures/log_jungle_top.png",
+        "textures/log_oak_top.png",
+        "textures/log_spruce_top.png"
+    ];
     
     let woolTextures = document.getElementsByClassName('wool');
     let planksTextures = document.getElementsByClassName('planks');
@@ -113,6 +196,13 @@ window.addEventListener("load", (event) => {
     let dyeConcreteTextures = document.getElementsByClassName('dye-concrete');
     let toolMaterialsTextures = document.getElementsByClassName('tools');
     let armorMaterialsTextures = document.getElementsByClassName('armor');
+    let dyeFlowersTextures = document.getElementsByClassName('dye-flower');
+    let doorMaterialsTextures = document.getElementsByClassName('door');
+    let minecartWithTextures = document.getElementsByClassName('minecart');
+    let pressurePlateTextures = document.getElementsByClassName('pressure-plate');
+    let slabTextures = document.getElementsByClassName('slab');
+    let buttonTextures = document.getElementsByClassName('button');
+    let woodLogTextures = document.getElementsByClassName('log');
 
     let woolCounter = 0;
     let planksCounter = 0;
@@ -121,6 +211,13 @@ window.addEventListener("load", (event) => {
     let dyeConcreteCounter = 0;
     let toolMaterialsCounter = 0;
     let armorMaterialsCounter = 0;
+    let dyeFlowersCounter = 0;
+    let doorMaterialsCounter = 0;
+    let minecartWithCounter = 0;
+    let pressurePlateCounter = 0;
+    let slabCounter = 0;
+    let buttonCounter = 0;
+    let woodLogCounter = 0;
 
     let timer = setInterval(() => {
         if(woolCounter > (woolImages.length - 1)) {
@@ -151,6 +248,34 @@ window.addEventListener("load", (event) => {
             armorMaterialsCounter = 0;
         }
 
+        if(dyeFlowersCounter > (dyeFlowersImages.length - 1)) {
+            dyeFlowersCounter = 0;
+        }
+
+        if(doorMaterialsCounter > (doorMaterialsImages.length - 1)) {
+            doorMaterialsCounter = 0;
+        }
+
+        if(minecartWithCounter > (minecartWithImages.length - 1)) {
+            minecartWithCounter = 0;
+        }
+
+        if(pressurePlateCounter > (pressurePlateImages.length - 1)) {
+            pressurePlateCounter = 0;
+        }
+
+        if(slabCounter > (slabImages.length - 1)) {
+            slabCounter = 0;
+        }
+
+        if(buttonCounter > (buttonImages.length - 1)) {
+            buttonCounter = 0;
+        }
+
+        if(woodLogCounter > (woodLogImages.length - 1)) {
+            woodLogCounter = 0;
+        }
+
         Array.from(woolTextures).forEach((element, index, array) => {
             woolTextures[index].src = woolImages[woolCounter];
         });
@@ -178,6 +303,34 @@ window.addEventListener("load", (event) => {
         Array.from(armorMaterialsTextures).forEach((element, index, array) => {
             armorMaterialsTextures[index].src = armorMaterialsImages[armorMaterialsCounter];
         });
+        
+        Array.from(dyeFlowersTextures).forEach((element, index, array) => {
+            dyeFlowersTextures[index].src = dyeFlowersImages[dyeFlowersCounter];
+        });
+
+        Array.from(doorMaterialsTextures).forEach((element, index, array) => {
+            doorMaterialsTextures[index].src = doorMaterialsImages[doorMaterialsCounter];
+        });
+
+        Array.from(minecartWithTextures).forEach((element, index, array) => {
+            minecartWithTextures[index].src = minecartWithImages[minecartWithCounter];
+        });
+
+        Array.from(pressurePlateTextures).forEach((element, index, array) => {
+            pressurePlateTextures[index].src = pressurePlateImages[pressurePlateCounter];
+        });
+
+        Array.from(slabTextures).forEach((element, index, array) => {
+            slabTextures[index].src = slabImages[slabCounter];
+        });
+
+        Array.from(buttonTextures).forEach((element, index, array) => {
+            buttonTextures[index].src = buttonImages[buttonCounter];
+        });
+
+        Array.from(woodLogTextures).forEach((element, index, array) => {
+            woodLogTextures[index].src = woodLogImages[woodLogCounter];
+        });
 
         woolCounter++;
         planksCounter++;
@@ -186,5 +339,12 @@ window.addEventListener("load", (event) => {
         dyeConcreteCounter++;
         toolMaterialsCounter++;
         armorMaterialsCounter++;
+        dyeFlowersCounter++;
+        doorMaterialsCounter++;
+        minecartWithCounter++;
+        pressurePlateCounter++;
+        slabCounter++;
+        buttonCounter++;
+        woodLogCounter++;
     }, 1500);  
 });
